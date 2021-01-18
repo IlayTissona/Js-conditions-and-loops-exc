8d6c7e48d5147f8e91a8634da1655a0ad6bf9249
IlayTissona <ilay211@gmail.com>
2021-01-18 13:11:50 +0200

    fixed question2
    noticed they asked for a product (*) and not sum (+)

--- q2.js
+++ q2.js
@@ -1,6 +1,6 @@
 let num =
-	parseInt(prompt("insert first number")) +
-	parseInt(prompt("insert secoond number")) +
+	parseInt(prompt("insert first number")) *
+	parseInt(prompt("insert secoond number")) *
 	parseInt(prompt("insert third number"));
 
 alert("the sign is: " + (num > 0 ? "+" : "-"));
